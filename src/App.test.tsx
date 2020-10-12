9a804aae36fbe86588f22c213a3f0fb529609746
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-10-12 21:50:16 +1100

    Feature spec

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
+import user from '@testing-library/user-event'
+import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('entering commands and obtaining an output of the robot location', () => {
+  it('outputs robot location', () => {
+    render(<App />)
+
+    const commandInputTextbox = screen.getByRole('textbox', { name: /command input/i })
+
+    user.type(commandInputTextbox, 'PLACE 2,2,NORTH{enter}')
+    user.type(commandInputTextbox, 'MOVE{enter}')
+    user.type(commandInputTextbox, 'REPORT')
+
+    const runButton = screen.getByRole('button', { name: /run/i })
+
+    user.click(runButton)
+
+    const output = screen.getByText('Output: 0,1,NORTH')
+    expect(output).toBeInTheDocument()
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
+  function onChange(e: any) {
+    return e.target.value
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <textarea aria-label="command input" onChange={onChange}></textarea>
+      <button>Run</button>
+      <summary role="summary"></summary>
+    </>
+  )
 }
 
-export default App;
+export default App
