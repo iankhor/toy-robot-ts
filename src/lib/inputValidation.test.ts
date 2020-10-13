665f85078392f2079ebac1c6e46a5aa43f822374
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-10-13 22:08:16 +1100

    a few more input validaiton specs

--- src/lib/inputValidation.test.ts
+++ src/lib/inputValidation.test.ts
@@ -5,15 +5,16 @@ const { PLACE, MOVE, LEFT, RIGHT, REPORT } = CommandInput
 
 describe('validateInput', () => {
   test.each`
-    commandInputs          | expectedOutput
-    ${[PLACE]}             | ${false}
-    ${['foobaz']}          | ${false}
-    ${['report']}          | ${true}
-    ${[REPORT]}            | ${true}
-    ${['PLACE 1,1,SOUTH']} | ${true}
-    ${[MOVE]}              | ${true}
-    ${[LEFT]}              | ${true}
-    ${[RIGHT]}             | ${true}
+    commandInputs                                          | expectedOutput
+    ${[PLACE]}                                             | ${false}
+    ${['foobaz']}                                          | ${false}
+    ${['report']}                                          | ${true}
+    ${[REPORT]}                                            | ${true}
+    ${['PLACE 1,1,SOUTH']}                                 | ${true}
+    ${[MOVE]}                                              | ${true}
+    ${[LEFT]}                                              | ${true}
+    ${[RIGHT, LEFT, REPORT]}                               | ${true}
+    ${[RIGHT, 'PLACE 1,1,SOUTH', LEFT, 'PLACE 1,1,SOUTH']} | ${true}
   `('validates $commandInputs, and returns $expectedOutput', ({ commandInputs, expectedOutput }) => {
     expect(validateInput(commandInputs.join('\n'))).toEqual(expectedOutput)
   })
