e983f0f2091a70834c032a473b858861957d3d53
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-10-13 19:46:23 +1100

    CSS tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import React, { useState, ChangeEvent } from 'react'
 import { run, Position, validateInput } from './lib'
+import './App.css'
+
+function Instruction(): JSX.Element {
+  return (
+    <pre>
+      Simple Instructions
+      <ol>
+        <li>Enter commands in the textbox below or click on the prefill an example button</li>
+        <li>Click on run</li>
+      </ol>
+      Example Commands
+      <ul>
+        <li>PLACE 2,3,NORTH</li>
+        <li>MOVE</li>
+        <li>LEFT</li>
+        <li>RIGHT</li>
+        <li>REPORT</li>
+      </ul>
+    </pre>
+  )
+}
+
+const EXAMPLE_COMMANDS = ['PLACE 2,2,NORTH', 'MOVE', 'MOVE', 'RIGHT', 'RIGHT', 'MOVE', 'REPORT']
 
 function App(): JSX.Element {
   const [commands, setCommands] = useState<string>('')
@@ -8,6 +31,10 @@ function App(): JSX.Element {
 
   function onChange(e: ChangeEvent<HTMLTextAreaElement>) {
     setCommands(e.target.value)
+  }
+
+  function prefillCommands() {
+    setCommands(EXAMPLE_COMMANDS.join('\n'))
   }
 
   function renderOuputs() {
@@ -25,12 +52,17 @@ function App(): JSX.Element {
   }
 
   return (
-    <>
-      <textarea aria-label="command input" value={commands || ''} onChange={onChange} />
-      <button onClick={runCommands}>Run</button>
-      {renderOuputs()}
-      {error && <div>Commands entered are incorrect, please enter a valid sequence of commands</div>}
-    </>
+    <div className="main center">
+      <Instruction />
+
+      <div className="command-input center">
+        {renderOuputs()}
+        {error && <div>Commands entered are incorrect, please enter a valid sequence of commands</div>}
+        <textarea aria-label="command input" value={commands || ''} onChange={onChange} />
+        <button onClick={prefillCommands}>Prefill an example</button>
+        <button onClick={runCommands}>Run</button>
+      </div>
+    </div>
   )
 }
 
